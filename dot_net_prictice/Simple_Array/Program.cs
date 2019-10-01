2153ef4f87c4e79d7b51c2f08a6178001ce0e8f3
itacademyhtp <itacademyhtp@gmail.com>
2019-10-01 21:28:34 +0300

    Done

---
+++ dot_net_prictice/Simple_Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[6];
+            Random rnd = new Random();
+
+            Console.WriteLine("Enter elements");
+            for (int index = 0; index < myArray.Length; index++)
+                myArray[index] = rnd.Next(1, 999);
+
+            PrintArray(myArray);
+
+            myArray[2] *= 10;
+
+            PrintArray(myArray);
+
+            Console.WriteLine("Lowest value");
+            Console.WriteLine(myArray.Min().ToString());
+
+            Console.ReadLine();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine("Printing Array");
+            foreach (int item in array)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
