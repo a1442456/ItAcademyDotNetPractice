363b67f6847c46ad1e3346775e677068d362cdcb
Silvanovich Egor <e.silva1998@mail.ru>
2019-10-24 20:28:40 +0300

    tasks complete

---
+++ Lecture_13/Song.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_13
+{
+    class Song
+    {
+        private string _songName;
+        private int _songLength;
+        private string _author;
+        private uint _year;
+
+        public Song(string songName, int songLength, string author, uint year)
+        {
+            _songName = songName;
+            _songLength = SongLength;
+            _author = author;
+            _year = year;
+        }
+
+        public uint Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public int SongLength
+        {
+            get { return _songLength; }
+            set { _songLength = value; }
+        }
+
+
+        public string SongName
+        {
+            get { return _songName; }
+            set { _songName = value; }
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "123456678";
+            Console.WriteLine(str.MyNewMethod());
+            Console.ReadLine();
+        }
+    }
+
+    static class MyOwnString
+    {
+        public static string MyNewMethod(this String str)
+        {
+            str = str.Substring(0, 5);
+            str += "...";
+            return str;
+        }
+    }
+}
